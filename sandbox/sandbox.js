0ad41be2cd05fc577d5151f29b7eae59ed06a642
PheonixBorn1820 <adamtfoerster@gmail.com>
2023-01-26 09:59:15 -0700

    Played around with Dialogs

---
+++ sandbox/sandbox.js
@@ -0,0 +1,11 @@
+const ok = document.querySelector('ok')
+const dialog = document.querySelector('dialog')
+const reset = document.querySelector('button')
+
+ok.addEventListener(click, () => {
+
+})
+
+reset.addEventListener(click, () => {
+    dialog.className = 'open'
+})
